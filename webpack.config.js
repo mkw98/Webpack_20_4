511fefee358cb4c8d777e44d042fb6ec29005caa
Maria Wieckowska <mkw98@gazeta.pl>
2018-10-20 23:52:57 +0200

    add files edit webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
 
-//webpack.config.js
 module.exports = {
     entry: './src/app.js',
-    output: {
+        output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            }
+        ]
     }
 };
