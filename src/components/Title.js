dee95035c485d6b148461807f11f2923417a4e86
Maria Wieckowska <mkw98@gazeta.pl>
2018-10-22 23:03:56 +0200

    add app.css edit title and index

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,1 +1,15 @@
-Title.js
+import React from 'react';
+
+const Title = props =>
+    <div>
+      <h1>{props.title}</h1>
+      <p>To do:</p>
+    </div>
+
+export default Title;
+
+/// function Title(props) {
+//    return (
+//        <h1>{props.title}</h1>
+ //   )
+//}
